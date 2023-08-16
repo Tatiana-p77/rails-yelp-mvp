b70f18b4f8b8aa904f788a0e40b8fcc14a14f642
Tatiana Parfait <tania.parfait@hotmail.fr>
2023-08-16 17:04:17 +0200

    p

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # voir la liste de tous les restaurants.
+  get 'restaurants', to: 'restaurants#index'
+
+  # ajouter un nouveau restaurant et être redirigé vers la vue show de la page de ce nouveau restaurant
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+
+  # voir les détails d’un restaurant, avec tous les avis associés au restaurant
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Un visiteur peut ajouter un nouvel AVIS à un restaurant.
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+
+restaurants_array = [
+  { name: 'Only Spaghetti 🍝', address: '1 place dItalie', phone_number: '1029', category: 'italian' },
+  { name: 'Pékin panda 🍚', address: '3 rue Chinatown', phone_number: '0987', category: 'chinese' },
+  { name: 'Tokyo sushi 🍣', address: '2 place Kyoto', phone_number: '1298', category: 'japanese' },
+  { name: 'Oui oui baguette 🥖', address: '5 rue de la Pompe', phone_number: '1234', category: 'french' },
+  { name: 'La Frite 🍟', address: '10 rue chocolat', phone_number: '1236', category: 'belgian' }
+]
+
+Restaurant.create(restaurants_array)
+
+puts '5 restaurants créés'
